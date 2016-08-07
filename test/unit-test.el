b27b17c55f2a9f7cace864be845f257a8e2f3d4a
Wilfred Hughes <me@wilfred.me.uk>
2016-08-07 09:34:15 -0400

    Write form offsets to a separate hash map.
    This means we can operate on normal forms, which is a lot less painful.

--- test/unit-test.el
+++ test/unit-test.el
@@ -1,34 +1,40 @@
 (require 'ert)
 (require 'refs)
+(require 'ht)
+
+(defun refs--read-all-from-string (string)
+  "Read all the forms from STRING, along with their offsets."
+  (with-temp-buffer
+    (insert string)
+    (refs--read-all-with-positions (current-buffer))))
 
 (ert-deftest refs-read-with-positions-whitespace ()
-  "We should not include leading whitespace when calculating form
-indexes."
-  (with-temp-buffer
-    (insert " foo ")
+  "Form offsets should be where the sexp starts, even if there's whitespace."
+  (-let [(forms offsets) (refs--read-all-from-string " foo ")]
     (should
-     (equal
-      (refs--read-with-positions (current-buffer) 0)
-      (list 'foo 1 4)))))
+     (equal forms '(foo)))
+    (should
+     (ht-equal? offsets (ht ('foo (list 1 4)))))))
 
 (ert-deftest refs-read-with-positions-recurse ()
-  (with-temp-buffer
-    (insert "(bar baz)")
+  "Ensure we return offsets of list items too."
+  (-let [(forms offsets) (refs--read-all-from-string "(bar baz)")]
     (should
-     (equal
-      (refs--read-with-positions (current-buffer) 0)
-      (list (list (list 'bar 1 4) (list 'baz 5 8)) 0 9)))))
+     (equal forms '((bar baz))))
+    (should
+     (ht-equal? offsets (ht ('(bar baz) (list 0 9))
+                            ('bar (list 1 4))
+                            ('baz (list 5 8)))))))
 
-(ert-deftest refs-read-all-with-positions ()
-  (with-temp-buffer
-    (insert "10 20 30")
+(ert-deftest refs-read-with-positions-multiple ()
+  "Ensure we read multiple sexps from the buffer."
+  (-let [(forms offsets) (refs--read-all-from-string "10 20 30")]
     (should
-     (equal
-      (refs--read-all-with-positions (current-buffer))
-      (list
-       (list 10 0 2)
-       (list 20 3 5)
-       (list 30 6 8))))))
+     (equal forms '(10 20 30)))
+    (should
+     (ht-equal? offsets (ht (10 (list 0 2))
+                            (20 (list 3 5))
+                            (30 (list 6 8)))))))
 
 (ert-deftest refs-find-calls ()
   "Ensure we can find top level calls and calls inside functions."
