a33cc9784dad364871582b9e70825cccac70d635
Wilfred Hughes <me@wilfred.me.uk>
2016-09-07 22:04:29 -0400

    Add a basic (currently failing) smoke test

--- test/unit-test.el
+++ test/unit-test.el
@@ -1,38 +1,6 @@
 (require 'ert)
 (require 'refs)
-(require 'ht)
 
-(defmacro with-temp-buffer-contents (string &rest body)
-  (declare (indent 1) (debug t))
-  `(with-temp-buffer
-     (insert ,string)
-     ,@body))
-
-(ert-deftest refs-paren-positions-basic ()
-  (with-temp-buffer-contents "( (foo) (bar) )"
-    (should
-     (equal
-      (refs--paren-positions (current-buffer) (point-min) (point-max))
-      '((3 8) (9 14))))))
-
-(ert-deftest refs-paren-positions-quoted ()
-  (with-temp-buffer-contents "(defcustom lispy-no-space nil\n  :group 'lispy)"
-    (should
-     (null
-      (refs--paren-positions (current-buffer) (point-min) (point-max))))))
-
-(ert-deftest refs-paren-positions-require ()
-  (with-temp-buffer-contents "(require 'foo)"
-    (should
-     (null
-      (refs--paren-positions (current-buffer) (point-min) (point-max))))))
-
-;; (ert-deftest refs-find-calls ()
-;;   "Ensure we can find top level calls and calls inside functions."
-;;   (-let [(forms offsets) (refs--read-all-with-offsets "(foo 1)\n(defun bar () (foo))")]
-;;     (should
-;;      (equal
-;;       (refs--find-calls forms 'foo)
-;;       (list
-;;        '(foo 1)
-;;        '(foo))))))
+(ert-deftest refs-function ()
+  "Smoke test for `refs-function'."
+  (refs-function 'format))
