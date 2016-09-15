0d2ee2731a6f3851fe63abdbb90c5357427ee85f
Wilfred Hughes <me@wilfred.me.uk>
2016-09-14 20:17:56 -0400

    Try a higher max-specpdl-size value on Travis

--- test/unit-test.el
+++ test/unit-test.el
@@ -4,7 +4,7 @@
 ;; For some reason, travis CI is setting this value too low, meaning
 ;; we can't recurse on deep sexp structures.
 (message "old max-specpdl-size: %s" max-specpdl-size)
-(setq max-specpdl-size 1000)
+(setq max-specpdl-size 2000)
 
 (defmacro with-temp-backed-buffer (contents &rest body)
   "Create a temporary file with CONTENTS, and evaluate BODY
