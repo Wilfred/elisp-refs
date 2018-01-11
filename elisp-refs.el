eee751a6120f925cdffcfbb6a4545e599b953e94
Wilfred Hughes <me@wilfred.me.uk>
2018-01-11 22:31:20 +0000

    Reduce how many dependencies we have
    This is the main blocker to #1.

--- elisp-refs.el
+++ elisp-refs.el
@@ -5,7 +5,7 @@
 ;; Author: Wilfred Hughes <me@wilfred.me.uk>
 ;; Version: 1.3
 ;; Keywords: lisp
-;; Package-Requires: ((dash "2.12.0") (f "0.18.2") (list-utils "0.4.4") (loop "1.2") (s "1.11.0"))
+;; Package-Requires: ((dash "2.12.0") (loop "1.2") (s "1.11.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -32,11 +32,10 @@
 
 ;;; Code:
 
-(require 'list-utils)
 (require 'dash)
-(require 'f)
 (require 'loop)
 (require 's)
+(require 'format)
 (eval-when-compile (require 'cl-lib))
 
 (defun elisp-refs--format-int (integer)
@@ -118,7 +117,7 @@ Internal implementation detail.")
              (nreverse forms)
            ;; Some unexpected error, propagate.
            (error "Unexpected error whilst reading %s position %s: %s"
-                  (f-abbrev elisp-refs--path) (point) err)))))))
+                  (abbreviate-file-name elisp-refs--path) (point) err)))))))
 
 (defun elisp-refs--walk (buffer form start-pos end-pos symbol match-p &optional path)
   "Walk FORM, a nested list, and return a list of sublists (with
@@ -157,7 +156,7 @@ START-POS and END-POS should be the position of FORM within BUFFER."
       (--each (-zip form subforms-positions)
         (-let [(subform subform-start subform-end) it]
           (when (or
-                 (and (consp subform) (not (list-utils-improper-p subform)))
+                 (and (consp subform) (format-proper-list-p subform))
                  (and (symbolp subform) (eq subform symbol)))
             (-when-let (subform-matches
                         (elisp-refs--walk
@@ -325,10 +324,10 @@ Where the file was a .elc, return the path to the .el file instead."
   (let ((elc-paths (-non-nil (mapcar #'-first-item load-history))))
     (-non-nil
      (--map
-      (let ((el-name (format "%s.el" (f-no-ext it)))
-            (el-gz-name (format "%s.el.gz" (f-no-ext it))))
-        (cond ((f-exists? el-name) el-name)
-              ((f-exists? el-gz-name) el-gz-name)
+      (let ((el-name (format "%s.el" (file-name-sans-extension it)))
+            (el-gz-name (format "%s.el.gz" (file-name-sans-extension it))))
+        (cond ((file-exists-p el-name) el-name)
+              ((file-exists-p el-gz-name) el-gz-name)
               ;; Ignore files where we can't find a .el file.
               (t nil)))
       elc-paths))))
@@ -468,7 +467,7 @@ propertize them."
   "Return a button that navigates to PATH."
   (with-temp-buffer
     (insert-text-button
-     (f-abbrev path)
+     (abbreviate-file-name path)
      :type 'elisp-refs-path-button
      'path path)
     (buffer-string)))
